60d2fdf6365532230bb2b14fb05db9465c76dbf5
sumit yadav <sumitbsc36871@gmail.com>
2024-02-29 18:28:03 +0530

    model evaluation and mlflow added

--- src/mlProject/config/configuration.py
+++ src/mlProject/config/configuration.py
@@ -1,9 +1,10 @@
 from mlProject.constants import *
-from mlProject.utils.common import read_yaml, create_directories
+from mlProject.utils.common import read_yaml, create_directories, save_json
 from mlProject.entity.config_entity import (DataIngestionConfig,
                                             DataValidationConfig,
                                             DataTransformationConfig,
-                                            ModelTrainerConfig)
+                                            ModelTrainerConfig,
+                                            ModelEvaluationConfig)
 
 
 
@@ -84,3 +85,24 @@ class ConfigurationManager:
         )
 
         return model_trainer_config
+    
+
+    def get_model_evaluation_config(self) -> ModelEvaluationConfig:
+        config = self.config.model_evaluation
+        params = self.params.ElasticNet
+        schema =  self.schema.TARGET_COLUMN
+
+        create_directories([config.root_dir])
+
+        model_evaluation_config = ModelEvaluationConfig(
+            root_dir=config.root_dir,
+            test_data_path=config.test_data_path,
+            model_path = config.model_path,
+            all_params=params,
+            metric_file_name = config.metric_file_name,
+            target_column = schema.name,
+            mlflow_uri="https://dagshub.com/sumit36871/Machine_Learning_project_with_MLFlow.mlflow"
+           
+        )
+
+        return model_evaluation_config
